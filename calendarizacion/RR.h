95aa241dfd4e0ccfbface06517eb32287ae45e5d
Yendry <yen.badilla@gmail.com>
2025-05-03 14:02:11 -0600

    Archivos faltantes del commit anterior

--- calendarizacion/RR.h
+++ calendarizacion/RR.h
@@ -5,15 +5,15 @@
 
 // Quantum fijo en microsegundos
 #define QUANTUM_USEC 500000
-
+#define MAX_CARROS 100
 // Arreglo de punteros a hilos listos (carros)
-extern Car* cola_listos[];
+extern Car* cola_listos[MAX_CARROS];
 
 // Cantidad de hilos en la cola de listos
 extern int num_listos;
 
 // Tiempos restantes por carro en microsegundos
-extern int tiempos_restantes[];
+extern int tiempos_restantes[MAX_CARROS]; //se utiliza para rastrear cuánto tiempo le queda a cada carro para completar su tarea en el algoritmo de Round Robin
 
 //-----FUnciones de RR.c-----//
 // Funcion que ejecuta cada hilo
--- calendarizacion/prueba_algoritmos.c
+++ calendarizacion/prueba_algoritmos.c
@@ -1,15 +1,45 @@
 // filepath: /home/vale/Escritorio/ProyectoSO/Proyecto1-SO/calendarizacion/prueba_algoritmos.c
 #include "calendarizador.h"
+#include "CEthreads.h"
 #include <stdio.h>
 #include <stdlib.h>
 
 int main() {
-    Carro carro1 = {1, 3, 0};  // ID: 1, Prioridad: 3
-    Carro carro2 = {2, 1, 0};  // ID: 2, Prioridad: 1
-    Carro carro3 = {3, 2, 0};  // ID: 3, Prioridad: 2
+    Car carro1 = {
+        .tid = 1,               // ID
+        .prioridad = 3,         // Priority
+        .stack = NULL,          // Default value
+        .done = 0,              // Default value
+        .lugar_inicio = LUGAR_IZQUIERDA, // Default side
+        .tipo = TIPO_NORMAL,    // Default car type
+        .velocidad = 0.0f,      // Default speed
+        .tiempo = 0             // Default time
+    };
+    
+    Car carro2 = {
+        .tid = 2,               // ID
+        .prioridad = 1,         // Priority
+        .stack = NULL,          // Default value
+        .done = 0,              // Default value
+        .lugar_inicio = LUGAR_IZQUIERDA, // Default side
+        .tipo = TIPO_NORMAL,    // Default car type
+        .velocidad = 0.0f,      // Default speed
+        .tiempo = 0             // Default time
+    };
+    
+    Car carro3 = {
+        .tid = 3,               // ID
+        .prioridad = 2,         // Priority
+        .stack = NULL,          // Default value
+        .done = 0,              // Default value
+        .lugar_inicio = LUGAR_IZQUIERDA, // Default side
+        .tipo = TIPO_NORMAL,    // Default car type
+        .velocidad = 0.0f,      // Default speed
+        .tiempo = 0             // Default time
+    };
 
     printf("Prueba del algoritmo de Prioridad:\n");
-    configurar_algoritmo(PRIORIDAD);
+    configurar_algoritmo(RR);
 
     ingresar_carro(&carro1);
     ingresar_carro(&carro2);
